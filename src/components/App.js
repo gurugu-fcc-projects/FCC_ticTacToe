f783f9c6cd5babff6921d8313194aac042633671
Peter Krevenets <peter.k.nets@gmail.com>
2017-06-03 11:12:16 +0300

    Change App from class to function component

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
 import '../style/app.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="app">
-        <div className="start-screen">
-          <h1>CHOOSE SIDE</h1>
-          <div className="start-screen_selection">
-            <span className="start-screen_selection_x">X</span>
-            <span className="start-screen_selection_y">O</span>
-          </div>
-          <div className="start-screen_selection_legend">
-            <span>:first</span><span>:second</span>
-          </div>
-        </div>
+const App = () => (
+  <div className="app">
+    <div className="start-screen">
+      <h1>CHOOSE SIDE</h1>
+      <div className="start-screen_selection">
+        <span className="start-screen_selection_x">X</span>
+        <span className="start-screen_selection_y">O</span>
       </div>
-    );
-  }
-}
+      <div className="start-screen_selection_legend">
+        <span>:first</span><span>:second</span>
+      </div>
+    </div>
+  </div>
+)
 
 export default App;
