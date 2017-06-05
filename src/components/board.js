5dda1ba376abb7e193d18e053ebe826a906e8626
Peter Krevenets <peter.k.nets@gmail.com>
2017-06-05 15:24:43 +0300

    Change from canvas to simple divs

--- src/components/board.js
+++ src/components/board.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import '../style/board.css';
 
+
 const Board = () => {
   const boardData = ['X','O','O','X',0,0,'X',0,'X'];
 
   return (
     <div className="game-screen-board">
+      <div className="crossline"></div>
       { boardData.map((cell, index) =>
-        <div key={index}>{cell === 0 ? '' : cell}</div>) }
+        <div className="cell" key={index}>{cell === 0 ? '' : cell}</div>) }
     </div>
   );
 };
--- src/components/game.js
+++ src/components/game.js
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Board from './board';
 import '../style/game.css';
 
-const Game = () => (
-  <div className="outer-shell">
-    <div className="game-screen">
-      <div className="game-screen-score">
-        <div className="game-screen-score-title">Player</div>
-        <div className="game-screen-score-player">0</div>
+class Game extends Component{
+  render() {
+    return (
+      <div className="outer-shell">
+        <div className="game-screen">
+          <div className="game-screen-score">
+            <div className="game-screen-score-title">Player</div>
+            <div className="game-screen-score-player">0</div>
+          </div>
+          <Board />
+          <div className="game-screen-score">
+            <div className="game-screen-score-title">CPU</div>
+            <div className="game-screen-score-cpu">0</div>
+          </div>
+        </div>
       </div>
-      <Board />
-      <div className="game-screen-score">
-        <div className="game-screen-score-title">CPU</div>
-        <div className="game-screen-score-cpu">0</div>
-      </div>
-    </div>
-  </div>
-);
+    );
+  }
+};
 
 export default Game;
