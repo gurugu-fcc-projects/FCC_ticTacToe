0fbb7eb4bc436f151add9631b0c2c5fe99639657
Peter Krevenets <peter.k.nets@gmail.com>
2017-06-06 17:48:18 +0300

    Move drawing a crossline into a separate function drawCrossLine

--- src/components/game.js
+++ src/components/game.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
 
-import Board from './board';
-import { crossLine } from '../utils/crossline';
+// import Board from './board';
+import { chooseCrossLine } from '../utils/crossline';
 import '../style/game.css';
+
+const setupBoard = () => {
+  const canvas = document.querySelector('.game-board');
+  const cx = canvas.getContext('2d');
+
+  //=== draw a board
+  cx.beginPath();
+  cx.moveTo(100, 0);
+  cx.lineTo(100, 300);
+  cx.moveTo(200, 0);
+  cx.lineTo(200, 300);
+  cx.moveTo(0, 100);
+  cx.lineTo(300, 100);
+  cx.moveTo(0, 200);
+  cx.lineTo(300, 200);
+  cx.stroke();
+}
 
 class Game extends Component{
   componentDidMount() {
-    crossLine(1);
+    setupBoard();
+    chooseCrossLine(7);
+    // crossLine(1);
   }
 
   render() {
     return (
       <div className="outer-shell">
-        <div className="game-screen">
-          <div className="game-screen-score">
-            <div className="game-screen-score-title">Player</div>
-            <div className="game-screen-score-player">0</div>
+        <div className="game">
+          <div className="game-score">
+            <div className="game-score-title">Player</div>
+            <div className="game-score-player">0</div>
           </div>
-          <Board />
-          <div className="game-screen-score">
-            <div className="game-screen-score-title">CPU</div>
-            <div className="game-screen-score-cpu">0</div>
+          <canvas className="game-board" height="300px" width="300px"></canvas>
+          <div className="game-score">
+            <div className="game-score-title">CPU</div>
+            <div className="game-score-cpu">0</div>
           </div>
         </div>
       </div>
